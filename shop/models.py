e9df8b07f16b48f23a884e92bed40247a7689b61
13155a1 <ppy040@gmail.com>
2024-03-25 20:30:10 +0900

    feat: store, item 모델 생성

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Store(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    name = models.CharField(max_length=50)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    count = models.IntegerField()
+    price = models.IntegerField()
+    image = models.ImageField(blank=True, upload_to='item_images/')
+
+    def __str__(self):
+        return self.name
