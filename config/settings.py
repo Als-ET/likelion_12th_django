ff97cbbc17c6ffe79f13cf520f04a7aaef929d87
13155a1 <ppy040@gmail.com>
2024-03-31 18:04:27 +0900

    feat: static 파일 루트 설정

--- config/settings.py
+++ config/settings.py
@@ -128,3 +128,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # media 루트 설정
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+# static 루트 설정
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
