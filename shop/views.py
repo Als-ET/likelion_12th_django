be09efc9966714785f5dc07d1e74b68a96104412
13155a1 <ppy040@gmail.com>
2024-04-08 12:36:02 +0900

    feat: item_list view 생성

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from .models import Item
 
-# Create your views here.
+# 전체 상품 리스트 반환
+def item_list(request):
+    if request.method == 'GET':
+        items = Item.objects.all()
+        data = []
+
+        for item in items:
+            data.append(
+                {
+                    'id': item.pk,
+                    'name': item.name,
+                    'store': item.store,
+                    'count': item.count,
+                    'price': item.price,
+                    'image': item.image
+                }
+            )
+        
+        return JsonResponse(data)
