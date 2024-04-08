48e8412677bd549b430b77bcaf54d6c7ac14a832
13155a1 <ppy040@gmail.com>
2024-04-08 14:30:21 +0900

    feat: create item 구현

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse, Http404
-from .models import Item
+from .models import Item, Store
+from django.views.decorators.csrf import csrf_exempt
 
 # 전체 상품 리스트 반환
+@csrf_exempt
 def item_list(request):
     if request.method == 'GET':
         items = Item.objects.all()
@@ -21,13 +23,30 @@ def item_list(request):
             )
         
         return JsonResponse(data=data, safe=False)
+    
+    if request.method == 'POST':
+        item = Item()
+        item.name = request.POST['name']
+        item.price = request.POST['price']
+        item.count = request.POST['count']
+        item.image = request.FILES['image']
+        store_name = request.POST['store']
+
+        try:
+            store = Store.objects.get(name=store_name)
+            item.store = store
+        except Store.DoesNotExist:
+            raise Http404('store does not exist') 
+        item.save()
+
+        return JsonResponse({"success":"item has been saved"})
 
 def item(request, pk):
     if request.method == 'GET':
         try:
             item = Item.objects.get(pk=pk)
         except Item.DoesNotExist:
-            raise Http404('board does not exist') 
+            raise Http404('item does not exist') 
         
         data = {
             'id': item.pk,
@@ -37,5 +56,5 @@ def item(request, pk):
             'price': item.price,
             'image': request.build_absolute_uri(item.image.url)
         }
-        
+
         return JsonResponse(data=data, safe=False)
